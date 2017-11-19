bf22c2f536d041582b063f07fa02f8acd0896528
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-19 20:57:29 +0900

    治りました！

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :views
+  resources :users, only: [:show]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
