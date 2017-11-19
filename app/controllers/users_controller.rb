1c3ed2efa2db5d0e3f26aaf092e42d8a06a88ac1
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-19 16:24:10 +0900

    訂正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user=User.find_by(id:1)
-    # TODO: user.find(params[:id])がエラー起きているため、とりあえず後で返信来たら解決する
+    @user=User.find(params[:id])
   end
 end
