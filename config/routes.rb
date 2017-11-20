2b7289a8902145ea55cf07ebbf35385347479a2b
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-20 17:20:37 +0900

    Finish user destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     :registrations => "registrations"
   }
   devise_for :views
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index, :destroy]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
