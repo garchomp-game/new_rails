25ed490f237a17230a1afcd20f2a623ca98988f7
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-21 01:08:07 +0900

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
